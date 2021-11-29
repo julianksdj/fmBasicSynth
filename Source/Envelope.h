865d85e378a26aef93d85291169eb2cb9139aa4d
juliks <66480309+julianksdj@users.noreply.github.com>
2021-11-29 21:40:11 +0100

    added Envelope class (part of the previous commit)

---
+++ Source/Envelope.h
@@ -0,0 +1,101 @@
+//
+//  Envelope.h
+//  fmsynth
+//
+//  Created by Keepin' Soul on 28/11/2021.
+//
+
+#ifndef Envelope_h
+#define Envelope_h
+
+class Envelope
+{
+
+public:
+    Envelope()
+    {
+        envCount = 0;
+        currentSampleRate = 48000;
+    };
+    
+    float getEnvelope()
+    {
+        //Calculate envelope
+        if(noteOn==true) //pressed note on the keyboard
+        {
+            if (envCount < attackSamples)
+            {
+                carrAmp+=(0.125/attackSamples); //the amplitude raises until max amplitude is reached
+            }
+            else if (envCount < daSamples)
+            {
+                carrAmp-=((0.125-sustainLevel)/decaySamples); //the amplitude decreases until sustain level is reached
+            }
+            releaseLevel = carrAmp; // saves the amplitude state
+        }
+        else //released note on the keyboard
+        {
+            if(carrAmp>0.0000001)
+                carrAmp-=(releaseLevel/releaseSamples); //amplitude decreases until silence
+        }
+        envCount++;
+        return carrAmp;
+    };
+    
+    void setAttack(float a)
+    {
+        attackTime = a;
+        attackSamples = currentSampleRate * attackTime;
+        printf("Attack time: %f\n", attackTime);
+        printf("Attack samples: %d\n", attackSamples);
+    };
+    void setDecay(float d)
+    {
+        decayTime = d;
+        decaySamples = currentSampleRate * decayTime;
+        printf("decay time %f\n", decayTime);
+        daSamples = decaySamples + attackSamples;
+        printf("decay Samples %d\n", decaySamples);
+        printf("da Samples %d\n", daSamples);
+    };
+    void setSustain(float s)
+    {
+        sustainLevel = s;
+    };
+    void setRelease(float r)
+    {
+        releaseTime = r;
+        releaseSamples = currentSampleRate * releaseTime;
+        printf("release level %f\n", releaseLevel);
+        printf("release time %f\n", releaseTime);
+        printf("release samples %d\n", releaseSamples);
+    };
+    void setSampleRate(int sr)
+    {
+        currentSampleRate = sr;
+    };
+    float getCarrAmp(){
+        return carrAmp;
+    };
+    void setCarrAmp(float a){
+        carrAmp=a;
+    };
+    void setNoteOn(bool n){
+        noteOn = n;
+    };
+    void resetEnvCount(){
+        envCount = 0;
+    }
+    
+private:
+    float currentSampleRate;
+    float carrAmp;
+    float attackTime, decayTime, sustainLevel, releaseTime;
+    int attackSamples, decaySamples, releaseSamples, daSamples;
+    int envCount;
+    bool noteOn;
+    float releaseLevel;
+};
+
+
+#endif /* Envelope_h */
